371793c5b47a78a06703e0b163e550b64c0d7219
Ching <94354399+Chinh00@users.noreply.github.com>
2024-10-26 17:34:17 +0700

    add identity server

--- src/Services/IdentityServer/Data/Domain/User.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace IdentityServer.Data.Domain;
-
-public class User : IdentityUser<Guid>
-{
-    
-}
--- src/Services/IdentityServer/UseCases/CustomerCreateModel.cs
+++
@@ -1,7 +0,0 @@
-namespace IdentityServer.UseCases;
-
-public class CustomerCreateModel
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
