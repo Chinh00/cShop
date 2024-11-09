69ec0babae7cc0d368a3df1356175629cb852278
Ching <94354399+Chinh00@users.noreply.github.com>
2024-11-09 11:34:44 +0700

    add identity service

---
+++ src/Services/IdentityServer/Identity.Api/Services/SigninManager.cs
@@ -0,0 +1,17 @@
+using Identity.Api.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace Identity.Api.Services;
+
+public class SigninManager(
+    UserManager<ApplicationUser> userManager,
+    IHttpContextAccessor contextAccessor,
+    IUserClaimsPrincipalFactory<ApplicationUser> claimsFactory,
+    IOptions<IdentityOptions> optionsAccessor,
+    ILogger<SignInManager<ApplicationUser>> logger,
+    IAuthenticationSchemeProvider schemes,
+    IUserConfirmation<ApplicationUser> confirmation)
+    : SignInManager<ApplicationUser>(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes,
+        confirmation);
---
+++ src/Services/IdentityServer/Identity.Api/Services/UserManager.cs
@@ -0,0 +1,18 @@
+using Identity.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace Identity.Api.Services;
+
+public class UserManager(
+    IUserStore<ApplicationUser> store,
+    IOptions<IdentityOptions> optionsAccessor,
+    IPasswordHasher<ApplicationUser> passwordHasher,
+    IEnumerable<IUserValidator<ApplicationUser>> userValidators,
+    IEnumerable<IPasswordValidator<ApplicationUser>> passwordValidators,
+    ILookupNormalizer keyNormalizer,
+    IdentityErrorDescriber errors,
+    IServiceProvider services,
+    ILogger<UserManager<ApplicationUser>> logger)
+    : UserManager<ApplicationUser>(store, optionsAccessor, passwordHasher, userValidators, passwordValidators,
+        keyNormalizer, errors, services, logger);
