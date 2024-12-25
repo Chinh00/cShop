699c46ecf22e6f1748ae919197407da117e96b2a
Ching <94354399+Chinh00@users.noreply.github.com>
2024-12-25 19:37:32 +0700

    update unittest

---
+++ test/Catalog.Application.Tests/UseCases/Command/ActiveCatalogCommandTest.cs
@@ -0,0 +1,15 @@
+using Application.UseCases.Command;
+
+namespace Catalog.Application.Tests.UseCases.Command;
+
+[TestFixture]
+[TestOf(typeof(ActiveCatalogCommand))]
+public class ActiveCatalogCommandTest
+{
+
+    [Test]
+    public void METHOD()
+    {
+        
+    }
+}
