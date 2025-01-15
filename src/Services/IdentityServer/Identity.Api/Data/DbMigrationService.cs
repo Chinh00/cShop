b49e2090abc81bbda16e2cd2600ab63775a4e2c1
Ching <hanvipno9@gmail.com>
2025-01-15 10:11:16 +0700

    update

--- src/Services/IdentityServer/Identity.Api/Data/DbMigrationService.cs
+++ src/Services/IdentityServer/Identity.Api/Data/DbMigrationService.cs
@@ -1,15 +1,23 @@
+using Polly;
 namespace Identity.Api.Data;
 
-public class DbMigrationService(IServiceProvider serviceProvider) : IHostedService
+public class DbMigrationService(IServiceProvider serviceProvider, ILogger<DbMigrationService> logger) : IHostedService
 {
 
     public async Task StartAsync(CancellationToken cancellationToken)
-    {
-        Console.WriteLine("Database Migration Service is starting.");
-        using var scope = serviceProvider.CreateScope();
-        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        await dbContext.Database.MigrateAsync(cancellationToken: cancellationToken);
-    }
+        => await PollyPolicy(5).ExecuteAsync(async () =>
+        {
+            Console.WriteLine("Database Migration Service is starting.");
+            using var scope = serviceProvider.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            await dbContext.Database.MigrateAsync(cancellationToken: cancellationToken);
+        });
 
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+    IAsyncPolicy PollyPolicy(int retryCount) => Policy.Handle<Exception>().WaitAndRetryAsync(retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(3, retryAttempt)),
+        (exception, timeSpan, rCount, context) =>
+        {
+            logger.LogError(exception.Message);
+        });
 }
