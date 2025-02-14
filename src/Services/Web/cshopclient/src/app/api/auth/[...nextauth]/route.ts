078d892b9e0af83434aa52a812a7a8f56ef32107
Ching <hanvipno9@gmail.com>
2025-02-14 16:50:47 +0700

    Update route.ts

--- src/Services/Web/cshopclient/src/app/api/auth/[...nextauth]/route.ts
+++ src/Services/Web/cshopclient/src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,9 @@
-
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 import type {AuthOptions} from "next-auth";
 import http from "@/utils/http"
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
     session: {
         strategy: 'jwt',
         maxAge: 5 * 60,
